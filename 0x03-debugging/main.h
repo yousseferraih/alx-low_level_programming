04e6f734a9f7d8fabd55d3982a8c6018056c8b40
yousseferraih <erraihyoussef@gmail.com>
2023-07-20 06:57:18 -0700

    largest num

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,3 @@
-cat main.h
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -6,5 +5,7 @@ cat main.h
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
